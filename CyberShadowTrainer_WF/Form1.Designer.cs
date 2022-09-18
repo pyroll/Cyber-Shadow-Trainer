2607953c06da36f362c918f333294afd092be480
pyroll <61872672+pyroll@users.noreply.github.com>
2022-09-18 14:55:22 -0500

    Add Current HP, SP, Money to Form

--- CyberShadowTrainer_WF/Form1.Designer.cs
+++ CyberShadowTrainer_WF/Form1.Designer.cs
@@ -43,6 +43,12 @@
             this.LoadPosBtn = new System.Windows.Forms.Button();
             this.SavedXPosTextBox = new System.Windows.Forms.TextBox();
             this.SavedYPosTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -67,7 +73,7 @@
             // CurrentPositionLabel
             // 
             this.CurrentPositionLabel.AutoSize = true;
-            this.CurrentPositionLabel.Location = new System.Drawing.Point(7, 31);
+            this.CurrentPositionLabel.Location = new System.Drawing.Point(7, 125);
             this.CurrentPositionLabel.Name = "CurrentPositionLabel";
             this.CurrentPositionLabel.Size = new System.Drawing.Size(96, 15);
             this.CurrentPositionLabel.TabIndex = 2;
@@ -76,7 +82,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(8, 53);
+            this.label2.Location = new System.Drawing.Point(8, 147);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(17, 15);
             this.label2.TabIndex = 3;
@@ -85,7 +91,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(8, 77);
+            this.label3.Location = new System.Drawing.Point(8, 171);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(17, 15);
             this.label3.TabIndex = 4;
@@ -94,7 +100,7 @@
             // XPosLabel
             // 
             this.XPosLabel.AutoSize = true;
-            this.XPosLabel.Location = new System.Drawing.Point(31, 53);
+            this.XPosLabel.Location = new System.Drawing.Point(31, 147);
             this.XPosLabel.Name = "XPosLabel";
             this.XPosLabel.Size = new System.Drawing.Size(28, 15);
             this.XPosLabel.TabIndex = 5;
@@ -103,7 +109,7 @@
             // YPosLabel
             // 
             this.YPosLabel.AutoSize = true;
-            this.YPosLabel.Location = new System.Drawing.Point(31, 77);
+            this.YPosLabel.Location = new System.Drawing.Point(31, 171);
             this.YPosLabel.Name = "YPosLabel";
             this.YPosLabel.Size = new System.Drawing.Size(28, 15);
             this.YPosLabel.TabIndex = 6;
@@ -116,7 +122,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(125, 31);
+            this.label4.Location = new System.Drawing.Point(125, 125);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(87, 15);
             this.label4.TabIndex = 7;
@@ -125,7 +131,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(125, 77);
+            this.label7.Location = new System.Drawing.Point(125, 171);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(17, 15);
             this.label7.TabIndex = 9;
@@ -134,7 +140,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(125, 53);
+            this.label8.Location = new System.Drawing.Point(125, 147);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(17, 15);
             this.label8.TabIndex = 8;
@@ -142,7 +148,7 @@
             // 
             // SavePosBtn
             // 
-            this.SavePosBtn.Location = new System.Drawing.Point(12, 100);
+            this.SavePosBtn.Location = new System.Drawing.Point(12, 194);
             this.SavePosBtn.Name = "SavePosBtn";
             this.SavePosBtn.Size = new System.Drawing.Size(93, 23);
             this.SavePosBtn.TabIndex = 12;
@@ -152,7 +158,7 @@
             // 
             // LoadPosBtn
             // 
-            this.LoadPosBtn.Location = new System.Drawing.Point(148, 100);
+            this.LoadPosBtn.Location = new System.Drawing.Point(148, 194);
             this.LoadPosBtn.Name = "LoadPosBtn";
             this.LoadPosBtn.Size = new System.Drawing.Size(93, 23);
             this.LoadPosBtn.TabIndex = 13;
@@ -162,23 +168,83 @@
             // 
             // SavedXPosTextBox
             // 
-            this.SavedXPosTextBox.Location = new System.Drawing.Point(148, 48);
+            this.SavedXPosTextBox.Location = new System.Drawing.Point(148, 142);
             this.SavedXPosTextBox.Name = "SavedXPosTextBox";
             this.SavedXPosTextBox.Size = new System.Drawing.Size(100, 23);
             this.SavedXPosTextBox.TabIndex = 14;
             // 
             // SavedYPosTextBox
             // 
-            this.SavedYPosTextBox.Location = new System.Drawing.Point(148, 73);
+            this.SavedYPosTextBox.Location = new System.Drawing.Point(148, 167);
             this.SavedYPosTextBox.Name = "SavedYPosTextBox";
             this.SavedYPosTextBox.Size = new System.Drawing.Size(100, 23);
             this.SavedYPosTextBox.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 15);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Current HP:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 48);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 15);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Current SP:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(7, 64);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(90, 15);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Current Money:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(103, 33);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(14, 15);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "#";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(103, 48);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(14, 15);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "#";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(103, 64);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(14, 15);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "#";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(272, 133);
+            this.ClientSize = new System.Drawing.Size(269, 227);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.SavedYPosTextBox);
             this.Controls.Add(this.SavedXPosTextBox);
             this.Controls.Add(this.LoadPosBtn);
@@ -219,5 +285,11 @@
         private Button LoadPosBtn;
         private TextBox SavedXPosTextBox;
         private TextBox SavedYPosTextBox;
+        private Label label5;
+        private Label label6;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
     }
 }
